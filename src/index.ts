61859d45f8ab8ae3b036c85eb885aa814e42ccea
Michael Braun <michael@web4brauns.de>
2023-02-24 23:03:54 +0100

    Add some analysis when uploading files

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 import http from 'node:http';
 import * as Url from 'node:url'
-import { mkdir, readFile, writeFile } from 'node:fs/promises';
-import path from 'node:path';
-import tar from 'tar';
-import isWithin from './utils/isWithin.js';
 import { UPLOAD_REGEX } from './constants/regexes.js';
-import { DATA_PATH } from './constants/app-constants.js';
-import updateMetaData from './utils/updateMetaData.js';
-import { createHash } from 'crypto';
+import handleUpload from './utils/handleUpload.js';
 
 const port = 3000;
 
@@ -30,71 +24,7 @@ const server = http.createServer(async (req, res) => {
 
         const projectId = uploadMatch[1];
 
-        const now = new Date();
-        const monthString = String(now.getUTCMonth()).padStart(2, '0');
-        const dateString = String(now.getUTCDate()).padStart(2, '0');
-        const hoursString = String(now.getUTCHours()).padStart(2, '0');
-        const minutesString = String(now.getUTCMinutes()).padStart(2, '0');
-        const secondsString = String(now.getUTCSeconds()).padStart(2, '0');
-
-        const versionKey = `${now.getUTCFullYear()}-${monthString}-${dateString}_${hoursString}:${minutesString}:${secondsString}.${now.getUTCMilliseconds()}`;
-
-        const basePath = path.resolve(DATA_PATH, projectId);
-        const versionPath = path.resolve(basePath, versionKey);
-        await mkdir(versionPath, {
-            recursive: true,
-        });
-
-        const tarFile = path.resolve(basePath, `${versionKey}.tar`);
-        await writeFile(tarFile, req);
-
-        let paths: string[] = [];
-
-        await tar.x({
-            file: tarFile,
-            cwd: versionPath,
-            filter: (path, stat) => {
-                if (!isWithin(versionPath, path)) {
-                    return false;
-                }
-
-                if ((stat as any).type === 'File') {
-                    paths.push(path.startsWith('./') ? path.substring(2) : path);
-                }
-
-                return true;
-            },
-        });
-
-        console.log('projectId', projectId, versionKey, paths);
-
-        const files: Record<string, { hash: string }> = {};
-        for (let i = 0, z = paths.length; i < z ; i += 1) {
-            const content = await readFile(path.resolve(versionPath, `./${paths[i]}`));
-            const hash = createHash('md5').update(content).digest('base64');
-            files[paths[i]] = {
-                hash
-            };
-        }
-
-        await updateMetaData(projectId, async (metaData) => {
-            metaData.versions[versionKey] = {
-                files,
-            };
-
-            paths.forEach((path) => {
-                metaData.files[path] ||= [];
-                metaData.files[path].push(versionKey);
-            });
-
-            metaData.currentVersion = versionKey;
-            metaData.versionsArray.push(versionKey);
-
-            return metaData;
-        });
-
-        res.statusCode = 201;
-        res.end();
+        await handleUpload(req, res, projectId);
         return;
     }
 
---
+++ src/utils/handleUpload.ts
@@ -0,0 +1,85 @@
+import path from 'node:path';
+import { DATA_PATH } from '../constants/app-constants.js';
+import { mkdir, readFile, writeFile } from 'node:fs/promises';
+import tar from 'tar';
+import isWithin from './isWithin.js';
+import { createHash } from 'crypto';
+import updateMetaData from './updateMetaData.js';
+import type { IncomingMessage, ServerResponse } from 'node:http';
+import { lookup as mimeLookup } from 'mime-types';
+
+export default async function handleUpload(req: IncomingMessage, res: ServerResponse & { req: IncomingMessage; }, projectId: string): Promise<void> {
+    const now = new Date();
+    const monthString = String(now.getUTCMonth()).padStart(2, '0');
+    const dateString = String(now.getUTCDate()).padStart(2, '0');
+    const hoursString = String(now.getUTCHours()).padStart(2, '0');
+    const minutesString = String(now.getUTCMinutes()).padStart(2, '0');
+    const secondsString = String(now.getUTCSeconds()).padStart(2, '0');
+
+    const versionKey = `${now.getUTCFullYear()}-${monthString}-${dateString}_${hoursString}:${minutesString}:${secondsString}.${now.getUTCMilliseconds()}`;
+
+    const basePath = path.resolve(DATA_PATH, projectId);
+    const versionPath = path.resolve(basePath, versionKey);
+    await mkdir(versionPath, {
+        recursive: true,
+    });
+
+    const tarFile = path.resolve(basePath, `${versionKey}.tar`);
+    await writeFile(tarFile, req);
+
+    let paths: { path: string; size: number; }[] = [];
+
+    await tar.x({
+        file: tarFile,
+        cwd: versionPath,
+        filter: (path, stat) => {
+            if (!isWithin(versionPath, path)) {
+                return false;
+            }
+
+            if ((stat as any).type === 'File') {
+                console.log(stat);
+
+                const normalizedPath = path.startsWith('./') ? path.substring(2) : path;
+
+                paths.push({
+                    path: normalizedPath,
+                    size: stat.size,
+                });
+            }
+
+            return true;
+        },
+    });
+
+    const files: Record<string, { hash: string; size: number; mime: string; }> = {};
+    for (let i = 0, z = paths.length; i < z ; i += 1) {
+        const content = await readFile(path.resolve(versionPath, `./${paths[i].path}`));
+        const hash = createHash('md5').update(content).digest('base64');
+        files[paths[i].path] = {
+            hash,
+            size: paths[i].size,
+            mime: mimeLookup(paths[i].path) || 'application/octet-stream',
+        };
+    }
+
+    await updateMetaData(projectId, async (metaData) => {
+        metaData.versions[versionKey] = {
+            files,
+        };
+
+        paths.forEach((path) => {
+            metaData.files[path.path] ||= [];
+            metaData.files[path.path].push(versionKey);
+        });
+
+        metaData.currentVersion = versionKey;
+        metaData.versionsArray.push(versionKey);
+
+        return metaData;
+    });
+
+    res.statusCode = 201;
+    res.end();
+    return;
+}
