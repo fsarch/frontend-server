2bf511871b0a23bf7f635d406d0ad7c92a5ccd00
Michael Braun <michael@web4brauns.de>
2024-10-26 17:20:55 +0200

    Remove unused code

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
-import http from 'node:http';
-import * as Url from 'node:url';
-
 import { NestFactory } from "@nestjs/core";
 import { VersioningType } from "@nestjs/common";
 
-import { PROJECT_REGEX } from './constants/regexes.js';
-import handleFile from './utils/handleFile.js';
 import { AppModule } from "./app.module.js";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
@@ -35,78 +30,6 @@ async function bootstrap() {
     await app.listen(PORT, () => {
         console.log(`Server running at port ${PORT}`);
     });
-
-    const server = http.createServer(async (req, res) => {
-        const url = Url.parse(req.url as string);
-
-        // if (url.pathname?.startsWith('/api')) {
-        //     const uploadMatch = url.pathname?.match(UPLOAD_REGEX);
-        //     if (!uploadMatch) {
-        //         res.statusCode = 404;
-        //         res.end();
-        //         return;
-        //     }
-        //
-        //     if (req.method !== 'POST') {
-        //         res.statusCode = 405;
-        //         res.end();
-        //         return;
-        //     }
-        //
-        //     if (!req.headers.authorization || !req.headers.authorization.toLowerCase().startsWith('secret ')) {
-        //         res.statusCode = 401;
-        //         res.end();
-        //         return;
-        //     }
-        //
-        //     const secret = req.headers.authorization.substring(7);
-        //
-        //     if (secret !== UPLOAD_SECRET) {
-        //         res.statusCode = 403;
-        //         res.end();
-        //         return;
-        //     }
-        //
-        //     const projectId = uploadMatch[1];
-        //
-        //     await handleUpload(req, projectId);
-        //
-        //     try {
-        //         await invalidateMetaData(projectId);
-        //     } catch (e) {
-        //         console.error('error while invalidating meta data', e);
-        //     }
-        //     return;
-        // }
-
-        // if (url.pathname?.startsWith('/projects')) {
-        //     const projectMatch = url.pathname?.match(PROJECT_REGEX);
-        //     if (!projectMatch) {
-        //         res.statusCode = 404;
-        //         res.end();
-        //         return;
-        //     }
-        //
-        //     if (req.method !== 'GET') {
-        //         res.statusCode = 405;
-        //         res.end();
-        //         return;
-        //     }
-        //
-        //     const projectId = projectMatch[1];
-        //     const resourcePath = projectMatch[2] ?? '';
-        //
-        //     await handleFile(req, res, projectId, resourcePath);
-        //     return;
-        // }
-        //
-        // res.statusCode = 404;
-        // res.end();
-    });
-
-    // server.listen(PORT, () => {
-    //     console.log(`Server running at port ${PORT}`);
-    // });
 }
 
 bootstrap();
