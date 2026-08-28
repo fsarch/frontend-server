04fb5ed000158ae677155669291abd4ce2f3aa7d
Michael Braun <michael@web4brauns.de>
2026-08-28 23:55:33 +0200

    Update `lru-cache` to v11.5.2 and adjust import statement in `file.service.ts`.

--- src/utils/file/file.service.ts
+++ src/utils/file/file.service.ts
@@ -2,7 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import type { Response } from 'express';
-import LRUCache from 'lru-cache';
+import { LRUCache } from 'lru-cache';
 import { lookup as mimeLookup } from 'mime-types';
 import path from 'node:path';
 import { MetadataService, ProjectFileInfo } from '../metadata/metadata.service.js';
